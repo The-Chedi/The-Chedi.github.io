d072e4fd8f0851a6a8260ef4d37f38d8d8f9ace5
The-Chedi <textgame91@gmail.com>
2026-05-03 21:56:39 -0500

    Update firebase-config.js

--- kitchen-system-order/firebase-config.js
+++ kitchen-system-order/firebase-config.js
@@ -1,1 +1,11 @@
-const firebaseConfig = { apiKey: "AIzaSyD57ErKNYwqGbLcriDYYvPUtVIw8jRncXQ", authDomain: "system-order-kitchen.firebaseapp.com", projectId: "system-order-kitchen", storageBucket: "system-order-kitchen.firebasestorage.app", messagingSenderId: "429224554052", appId: "1:429224554052:web:85d32727439fe55efcd611" }; // Initialize Firebase const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyD57ErKNYwqGbLcriDYYvPUtVIw8jRncXQ",
+  authDomain: "system-order-kitchen.firebaseapp.com",
+  projectId: "system-order-kitchen",
+  storageBucket: "system-order-kitchen.firebasestorage.app",
+  messagingSenderId: "429224554052",
+  appId: "1:429224554052:web:85d32727439fe55efcd611"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
