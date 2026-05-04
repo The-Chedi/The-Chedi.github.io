c7a32a4eedf12571323a8afb2ef5e2ada129a59d
The-Chedi <textgame91@gmail.com>
2026-05-03 22:04:15 -0500

    Update firebase-config.js

--- kitchen-system-order/firebase-config.js
+++ kitchen-system-order/firebase-config.js
@@ -4,8 +4,9 @@ const firebaseConfig = {
   projectId: "system-order-kitchen",
   storageBucket: "system-order-kitchen.firebasestorage.app",
   messagingSenderId: "429224554052",
-  appId: "1:429224554052:web:85d32727439fe55efcd611"
+  appId: "1:429224554052:web:7536c4cdea334679fcd611"
+
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
